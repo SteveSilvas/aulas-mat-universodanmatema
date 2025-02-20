a653c886e8f17f63d04754fa5f91804b8c59deea
Steve Ferreira Da Silva <steve.silva@gpssa.com.br>
2025-02-20 20:24:23 -0300

    ajustes de layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,38 @@
-import { Open_Sans } from "next/font/google";
-import "./globals.css";
+import { Open_Sans } from 'next/font/google';
+import './globals.css';
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
-import ClientLayout from "@/components/ClientLayout"; 
+import Navbar from "@/components/Navbar";
+import ToastComponent from '@/components/Toast';
 
 const openSans = Open_Sans({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-  variable: "--font-default",
+  subsets: ['latin'],
+  weight: ['400', '600', '700'],
+  variable: '--font-default',
 });
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
     <html lang="pt-br">
       <head>
-        <title>Aulas com Dan Matema</title>
+        <title>Dan Matema</title>
         <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={`${openSans.variable} font-sans flex flex-col min-h-screen`}>
         <header className="w-full fixed top-0 left-0 z-50">
+          <div className="fixed z-50 right-0">
+            {<ToastComponent />}
+          </div>
           <Header />
+          <Navbar />
         </header>
-        
-        <ClientLayout>
-          <main className="flex-grow w-full mt-[100px]">{children}</main>
-        </ClientLayout>
-
+        <main className="flex-grow w-full mt-[80px]">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
--- src/components/Toast/index.tsx
+++ src/components/Toast/index.tsx
@@ -1,22 +1,32 @@
-
+"use client";
 import { Toast, ToastToggle } from "flowbite-react";
 import { RiMessage2Fill } from "react-icons/ri";
+import { useAtom } from 'jotai';
+import { useEffect } from "react";
+import toastContext from "@/context/ToastAtom";
 
-export interface IToastComponent {
-    isOpen?: boolean;
-    text: string;
-}
-const ToastComponent: React.FC<IToastComponent> = ({
-    isOpen = true,
-    text,
+const ToastComponent: React.FC = ({
 }) => {
-    return isOpen ? (
+    const [toast, setToast] = useAtom(toastContext);
+
+    useEffect(() => {
+        if (toast.isOpen) {
+            setTimeout(() => {
+                setToast({
+                    isOpen: false,
+                    text: ''
+                });
+            }, 5000);
+        }
+    }, [toast, setToast]);
+
+    return toast.isOpen ? (
         <Toast>
             <div className="inline-flex h-8 w-8 shrink-0 items-center justify-center rounded-lg bg-cyan-100 text-cyan-500 dark:bg-cyan-800 dark:text-cyan-200">
                 <RiMessage2Fill className="h-5 w-5" />
             </div>
             <div className="ml-3 text-sm font-normal">
-                {text}
+                {toast.text}
             </div>
             <ToastToggle />
         </Toast>
